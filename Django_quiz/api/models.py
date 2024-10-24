a5e728ac5da57d5dc049eadb5678cde57cee829f
Eidrian2005 <eserrano@fwdcostarica.com>
2024-10-24 14:23:06 -0600

    terminado

--- Django_quiz/api/models.py
+++ Django_quiz/api/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Ubicacion(models.Model):
-    provincia = models.CharField(max_length=255, unique=True)
-    ciudad = models.CharField(max_length=255, unique=True)
-    calle = models.CharField(max_length=255, unique=True)
+    provincia = models.CharField(max_length=255, unique=True,blank=False)
+    ciudad = models.CharField(max_length=255, unique=True, blank=False)
+    calle = models.CharField(max_length=255, unique=True, blank=False)
 
     def __str__(self):
         return self.provincia
 
 class Hotel(models.Model):
-    ubicacion = models.ForeignKey(Ubicacion,on_delete=models.CASCADE)
-    hotel = models.CharField(max_length=255)
-    contacto = models.IntegerField(unique=True)
+    ubicacion = models.ForeignKey(Ubicacion,on_delete=models.CASCADE, blank=False)
+    hotel = models.CharField(max_length=255 ,blank=False)
+    contacto = models.IntegerField(unique=True, blank=False)
 
     def __str__(self):
         return self.hotel
@@ -28,18 +28,18 @@ class Categoria(models.Model):
 
 
 class Habitaciones(models.Model):
-    hotel = models.ForeignKey(Hotel,on_delete=models.CASCADE)
-    categoria = models.ManyToManyField(Categoria)   
-    numero_habitacion = models.IntegerField(unique=True)
+    hotel = models.ForeignKey(Hotel,on_delete=models.CASCADE,blank=False)
+    categoria = models.ManyToManyField(Categoria, blank=False)   
+    numero_habitacion = models.IntegerField(unique=True,blank=False)
 
     def __str__(self):
         return  str(self.numero_habitacion)
     
 
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=500)
-    apellido = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=500,blank=False)
+    apellido = models.CharField(max_length=100,blank=False)
+    direccion = models.CharField(max_length=255,blank=False)
     telefono = models.IntegerField()
     
     def __str__(self):
@@ -48,21 +48,21 @@ class Clientes(models.Model):
 
 
 class Reservas(models.Model):
-    clientes = models.ForeignKey(Clientes,on_delete=models.CASCADE)
-    habitaciones = models.ForeignKey(Habitaciones, on_delete=models.CASCADE)
-    fecha_entrada = models.DateTimeField(auto_now_add=True)
-    fecha_salida = models.DateTimeField()
+    clientes = models.ForeignKey(Clientes,on_delete=models.CASCADE,blank=False)
+    habitaciones = models.OneToOneField(Habitaciones, on_delete=models.CASCADE,blank=False)
+    fecha_entrada = models.DateTimeField(auto_now_add=True,blank=False)
+    fecha_salida = models.DateTimeField(blank=False)
     
     def __str__(self):
-        return self.fecha_entrada
+        return str( self.fecha_entrada)
 
 
 class Pagos(models.Model):
-    reservas = models.ForeignKey(Reservas, on_delete=models.CASCADE)
+    reservas = models.ForeignKey(Reservas, on_delete=models.CASCADE,blank=False)
     precio = models.IntegerField()
     fecha_pago = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.precio
+        return str(self.precio)
 
 
